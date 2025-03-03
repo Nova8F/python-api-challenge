7a10f83e10985f512d9159fa0c57b0631bc59b5f
Rayshawn <rayshawnmr99@gmail.com>
2025-03-03 13:53:33 -0500

    work inprogress

--- Starter_Code (6)/Starter_Code/api_keys.py
+++ Starter_Code (6)/Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "4792b873ec0b86f14505184e52f1923a "
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "78a712060d7c4e8cb482962bbb1d2827"
